b4613a0fd713e3783fa191a2afe22c30ac3265a5
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-02-18 12:18:21 +0100

    Commit

--- Boletin20_2/src/boletin20_2/TrianguloTartaglia.java
+++ Boletin20_2/src/boletin20_2/TrianguloTartaglia.java
@@ -8,20 +8,30 @@ public class TrianguloTartaglia {
         if (n > 0) {
             matriz = new int[n][];
             matriz[0] = new int[]{1};
-            System.out.println(matriz[0][0]+"  ");
-
+            for (int j = 0; j <= matriz.length; j++) {
+                System.out.print("  ");
+            }
+            System.out.println(matriz[0][0] + "  ");
+            int cont = matriz.length;
             for (int i = 1; i < matriz.length; i++) {
                 matriz[i] = new int[i + 1];
                 matriz[i][0] = matriz[i][i] = 1;
-                System.out.print( 1+"  ");
-                
+
+                for (int k = cont; k > 0; k--) {
+                    System.out.print("  ");
+                }
+                cont--;
+                System.out.print(1 + "   ");
+
                 for (int j = 1; j < i; j++) {
                     matriz[i][j] = matriz[i - 1][j - 1] + matriz[i - 1][j];
-                    System.out.print(matriz[i][j]+"  ");
+                    System.out.print(matriz[i][j] + "   ");
                 }
-                System.out.println(1+"  ");
+                System.out.println(1);
             }
 
         }
+
     }
+
 }
